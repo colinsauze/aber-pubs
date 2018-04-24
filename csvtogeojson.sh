4b53f6b62c60e93b8e6439378ecf2f703e5990c3
Colin Sauze <cos@aber.ac.uk>
2018-04-24 12:53:26 +0100

    changing format to include creator and comment

--- csvtogeojson.sh
+++ csvtogeojson.sh
@@ -9,14 +9,14 @@ for linenum in $(seq 2 $lines) ; do
     line=$(head -n $linenum $1 | tail -1)
     echo "Line: $linenum"
     echo $line
-    markercolour=$(echo $line | cut -d, -f 1)
-    markersize=$(echo $line | cut -d, -f 2)
-    markersymbol=$(echo $line | cut -d, -f 3)
-    name=$(echo $line | cut -d, -f 4)
+    name=$(echo $line | cut -d, -f 1)
+    markersymbol=$(echo $line | cut -d, -f 2)
+    creator=$(echo $line | cut -d, -f 3)
+    comment=$(echo $line | cut -d, -f 4)
     lon=$(echo $line | cut -d, -f 5)
     lat=$(echo $line | cut -d, -f 6)
     
-    echo  "{ \"type\": \"Feature\", \"properties\": { \"marker-color\": \"$markercolour\", \"marker-size\": \"$markersize\", \"marker-symbol\": \"$markersymbol\", \"name\": \"$name\" }," >> $2
+    echo  "{ \"type\": \"Feature\", \"properties\": { \"marker-size\": \"medium\", \"marker-symbol\": \"$markersymbol\", \"name\": \"$name\", \"creator\" : \"$creator\", \"comment\" : \"$comment\" }," >> $2
     
     #don't put a trailing comma on the last line
     if [ "$linenum" = "$lines" ] ; then
